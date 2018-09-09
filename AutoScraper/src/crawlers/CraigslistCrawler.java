9215b18f2cd0e3789a2a8d3c17430654ab5902ce
zbishop3455 <zbishop3455@icloud.com>
2018-09-08 23:58:18 -0400

    Added types to craigslist crawler

--- AutoScraper/src/crawlers/CraigslistCrawler.java
+++ AutoScraper/src/crawlers/CraigslistCrawler.java
@@ -14,25 +14,41 @@ import java.util.LinkedList;
 public class CraigslistCrawler extends ThreadedCrawler {
 
 	// all crawled links must contain this string
-	private String urlRestriction;
+	private String urlRestriction = "craigslist";
 
 	// sets the location of restriction
-	private String locationRestriction;
+	private String locationRestriction = "indianapolis";
 
 	// starting url
 	private String startingUrl;
 
+	// if a type is set, it will restrice the types of urls that are crawled
+	private CraigslistCrawlerType type = null;
+
 
 	public CraigslistCrawler(){
-		this.urlRestriction = "craigslist";
-		this.locationRestriction = "indianapolis";
-		this.startingUrl = "https://indianapolis.craigslist.org/";
+		this.init();
 	}
-
 
 
 	public CraigslistCrawler(String location){
 		this.locationRestriction = location;
+		this.init();
+	}
+
+
+	public CraigslistCrawler(CraigslistCrawlerType t){
+		this.urlRestriction = "craigslist";
+		this.init();
+	}
+
+
+
+	private void init(){
+
+		this.foundUrls = new LinkedList<String>();
+
+		this.startingUrl ="https://" + this.locationRestriction + ".craigslist.org/";
 	}
 
 
@@ -57,12 +73,14 @@ public class CraigslistCrawler extends ThreadedCrawler {
 
 	public void setStartingUrl(String url){
 		this.startingUrl = url;
+
 	}
 
 
-
-
-
+	public void setMaxUrls(int number) throws Exception{
+		if ( 0 < number) this.maxUrls = number;
+		else throw new Exception("Invalid Number");
+	}
 
 
 
---
+++ AutoScraper/src/crawlers/CraigslistCrawlerType.java
@@ -0,0 +1,11 @@
+// Author: Zachary Bishop
+// Created: 9/8/18
+// Purpose: streamline the use of crawler types for the Craigslist Crawler
+
+
+package crawlers;
+
+public enum CraigslistCrawlerType {
+	CAR,
+	MOTORCYCLE;
+}
