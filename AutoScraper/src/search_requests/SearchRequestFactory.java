fb15605f5b95818d932918c6a2508f49742f4410
Flarp <harpclan3475@gmail.com>
2018-11-01 20:48:47 -0400

    Committing the new files that we created to make the project more complete

---
+++ AutoScraper/src/search_requests/SearchRequestFactory.java
@@ -0,0 +1,13 @@
+/*
+ * Author: Adam Harpenau
+ * 11/1/18
+ * The purpose of this is to complete the factory method to obtain the specific search request
+ * sites
+ */
+
+package search_requests;
+
+public class SearchRequestFactory 
+{
+
+}
